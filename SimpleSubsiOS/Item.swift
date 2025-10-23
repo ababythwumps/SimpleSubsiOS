807222df5f59be17701c7bc0b1aa6ae88ad585c5
ababythwumps <michaelaedan@duck.com>
2025-10-23 11:04:01 -0700

    Initial Commit

---
+++ SimpleSubsiOS/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SimpleSubsiOS
+//
+//  Created by Michael Kao on 10/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SimpleSubsiOSTests/SimpleSubsiOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleSubsiOSTests.swift
+//  SimpleSubsiOSTests
+//
+//  Created by Michael Kao on 10/23/25.
+//
+
+import Testing
+
+struct SimpleSubsiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
